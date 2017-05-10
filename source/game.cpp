92d0864ce3d6a32e2c3a9706f4880ec5ace3fc56
Kieran Brownfield <kieranbrownfield@gmail.com>
2017-05-10 11:45:16 +0100

    Added code to create a window

--- source/game.cpp
+++ source/game.cpp
@@ -1,11 +1,26 @@
 #include "game.h"
 #include <iostream>
 #include <SDL.h>
+#include "Windows.h"
 
 void game_main()
 {
 	std::cout << "Hello from game" << std::endl;
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+	{
+		std::cout << "Unable to initialise SDL" << std::endl;
+		return;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Metal Warriors",
+		10, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+	if (window != nullptr)
+	{
+		Sleep(5000);
+
+		SDL_DestroyWindow(window);
+	}
 
 	SDL_Quit();
 
