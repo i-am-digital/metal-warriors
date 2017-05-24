89e472ae50f4b033b279b675da9024fc4321ee61
Byran Wills-Heath <byran@warriors.com>
2017-05-24 18:04:46 +0100

    Changed travis-build.sh to use g++-6

--- travis-build.sh
+++ travis-build.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -G"CodeBlocks - Unix Makefiles" && make
+cmake -G"CodeBlocks - Unix Makefiles" -DCMAKE_CXX_COMPILER=g++-6 .. && make
 cd tests
 ./metal-warriors.tests
